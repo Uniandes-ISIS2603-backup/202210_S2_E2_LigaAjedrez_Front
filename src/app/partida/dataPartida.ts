cf2f0d4fa2757c032a4964f9111a963b9521fbae
Xavi85 <j.serranor2@uniandes.edu.co>
2022-05-12 09:12:16 -0500

    push

--- src/app/partida/dataPartida.ts
+++ src/app/partida/dataPartida.ts
@@ -3,10 +3,22 @@ import { Liga } from "../liga/liga";
 import { Torneo } from "../torneo/torneo";
 import { Partida } from "./partida";
 
+var nom_liga = new Liga("Liga_1", "Bogotá", "Carrera 7", 22020254, "@ligaAjedrez");
+
+var nom_torneo1 = new Torneo("Torneo_1", 'Bogotá', new Date(12/10/2022), "100.000", nom_liga);
+var nom_torneo2 = new Torneo("Torneo_2", 'Cali', new Date(13/10/2022), "120.000", nom_liga);
+var nom_torneo3 = new Torneo("Torneo_3", 'Medellín', new Date(14/10/2022), "150.000", nom_liga);
+var nom_torneo4 = new Torneo("Torneo_4", 'Cartagena', new Date(15/10/2022), "90.000", nom_liga);
+
+var nom_apertura1 = new Apertura(1,"Inglesa","Cerrada","1. d4 d5 2. f4 Nf6 3. Nf3 c5 4. c3 Nc6");
+var nom_apertura2 = new Apertura(4,"Española","Abierta","1. d4 c6 2. f4 d5 3. Nf3 Bf5 4. e3 Nf6");
+var nom_apertura3 = new Apertura(5,"Stonewall","Super Loca","1. d4 e6 2. e4 d6 3. f4 Be7 4. Nf3 Bd7");
+var nom_apertura4 = new Apertura(6,"Cangrejo","Categoria Maestro Mundial","1. d4 d5 2. f4 Nf6 3. Nf3 e6 4. e3 Be7");
+
 export const dataPartida = [
 
-  new Partida(1, new Torneo("Torneo_1", 'Bogotá', new Date(12/10/2022), "100.000", new Liga("Liga_1", "Bogotá", "Carrera 7", 22020254, "@ligaAjedrez")), new Apertura(1,"Inglesa","Cerrada","1. d4 d5 2. f4 Nf6 3. Nf3 c5 4. c3 Nc6"), "Gano Blanco"),
-  new Partida(2, new Torneo("Torneo_2", 'Cali', new Date(13/10/2022), "120.000", new Liga("Liga_1", "Bogotá", "Carrera 7", 22020254, "@ligaAjedrez")), new Apertura(4,"Española","Abierta","1. d4 c6 2. f4 d5 3. Nf3 Bf5 4. e3 Nf6"), "Gano Negro"),
-  new Partida(3, new Torneo("Torneo_3", 'Medellín', new Date(14/10/2022), "150.000", new Liga("Liga_1", "Bogotá", "Carrera 7", 22020254, "@ligaAjedrez")), new Apertura(5,"Stonewall","Super Loca","1. d4 e6 2. e4 d6 3. f4 Be7 4. Nf3 Bd7"), "Gano Blanco"),
-  new Partida(4, new Torneo("Torneo_4", 'Cartagena', new Date(15/10/2022), "90.000", new Liga("Liga_1", "Bogotá", "Carrera 7", 22020254, "@ligaAjedrez")), new Apertura(6,"Cangrejo","Categoria Maestro Mundial","1. d4 d5 2. f4 Nf6 3. Nf3 e6 4. e3 Be7"), "Gano Negro")
+  new Partida(1, nom_torneo1, nom_apertura1, "Gano Blanco"),
+  new Partida(2, nom_torneo2, nom_apertura1, "Gano Negro"),
+  new Partida(3, nom_torneo3, nom_apertura1, "Gano Blanco"),
+  new Partida(4, nom_torneo4, nom_apertura1, "Gano Negro")
 ]
