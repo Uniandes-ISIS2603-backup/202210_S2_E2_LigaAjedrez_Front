b0f137a05483699647106dc655cf5bc3e0eef951
Sergio <s.arangoa@uniandes.edu.co>
2022-05-11 20:26:54 -0500

    quito routing de Jugada

--- src/app/jugada/jugada.module.ts
+++ src/app/jugada/jugada.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JugadaDetailComponent } from './jugada-detail/jugada-detail.component';
 import { JugadaListComponent } from './jugada-list/jugada-list.component';
-import { JugadaRoutingModule } from './jugada-routing.module';
+//import { JugadaRoutingModule } from './jugada-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, JugadaRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   exports: [JugadaListComponent],
   declarations: [JugadaListComponent, JugadaDetailComponent]
 })
