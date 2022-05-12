db27c311c4c2b27dab2b4b5aacd97863df0c8059
Jairo Nicolás Gómez <jn.gomezm1@uniandes.edu.co>
2022-05-12 01:14:30 -0500

    push #5

--- src/app/jugador/jugador-list/jugador-list.component.ts
+++ src/app/jugador/jugador-list/jugador-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { dataJugador } from '../dataJugador';
+import { Jugador } from '../jugador';
 
 import { JugadorService } from '../jugador.service';
 
@@ -9,9 +11,23 @@ import { JugadorService } from '../jugador.service';
 })
 export class JugadorListComponent implements OnInit {
 
+  jugadores: Array<Jugador> = [];
+  selected = false;
+  selectedJugador!: Jugador;
+
   constructor(private jugadorService: JugadorService) { }
 
+  getJugadorList(): Array<Jugador>{
+    return dataJugador;
+  }
+
+  onSelected(jugador: Jugador): void{
+    this.selected = true;
+    this.selectedJugador = jugador;
+  }
+
   ngOnInit() {
+    this.jugadores = this.getJugadorList();
   }
 
 }
