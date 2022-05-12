400402786e63af58039f18d43e8646650c2ab338
Xavi85 <j.serranor2@uniandes.edu.co>
2022-05-12 13:56:44 -0500

    push

--- src/app/jugada/jugada.service.ts
+++ src/app/jugada/jugada.service.ts
@@ -1,22 +1,9 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { Jugada } from './jugada';
-import { JugadaDetail } from './jugada-detail';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JugadaService {
 
-  private apiUrl: string = environment.baseUrl + 'jugadas';
-
-  constructor(private http: HttpClient) { }
-
-  getJugadas(): Observable<Jugada[]>
-  {
-    return this.http.get<Jugada[]>(this.apiUrl);
-  }
-
+  constructor() { }
 }
