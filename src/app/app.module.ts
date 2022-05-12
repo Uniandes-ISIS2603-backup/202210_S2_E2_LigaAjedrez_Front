38cbde7e2225987bb41fa3e8498b1def9ba83fcb
jcalixto202020774 <j.calixto@uniandes.edu.co>
2022-05-11 19:41:11 -0500

    apertura en app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PartidaModule } from './partida/partida.module';
+import { AperturaModule } from './apertura/apertura.module';
 
 @NgModule({
   declarations: [
