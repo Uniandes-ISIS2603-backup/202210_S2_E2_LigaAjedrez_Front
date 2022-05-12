2103a86a2f32c992aaca1244c4b75d2a50058fb9
Daniel Osorio Cardenas <d.osorioc2@uniandes.edu.co>
2022-05-11 22:50:27 -0500

    pUSHHH

--- src/app/comentario/comentario-partida-detail.ts
+++ src/app/comentario/comentario-partida-detail.ts
@@ -3,15 +3,12 @@ import { Comentario } from "./comentario";
 
 export class ComentarioPartidaDetail extends Comentario{
 
-  partidas: Array<PartidaDetail> = [];
-
   constructor(
     id: number,
     valoracionCualitativa: string,
     valoracionCuantitativa: number,
     autor: string,
-    fechaPublicacion: any,
-    partidas: Array<PartidaDetail>
+    fechaPublicacion: any
    ) {
     super(
       id,
@@ -20,6 +17,5 @@ export class ComentarioPartidaDetail extends Comentario{
       autor,
       fechaPublicacion
      )
-    this.partidas = partidas;
   }
 }
