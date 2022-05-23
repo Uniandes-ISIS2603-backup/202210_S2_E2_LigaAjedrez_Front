7d1ea96f46ec211ea3d5cb8aff3962919f0de141
Daniel Osorio Cardenas <d.osorioc2@uniandes.edu.co>
2022-05-23 02:30:56 -0500

    Ensayos pagina principal

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,10 +19,10 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-  it(`should have as title 'LigaAjedrez'`, () => {
+  it(`should have as title 'ligaajedrez-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LigaAjedrez');
+    expect(app.title).toEqual('ligaajedrez-front');
   });
 /*
   it('should render title', () => {
