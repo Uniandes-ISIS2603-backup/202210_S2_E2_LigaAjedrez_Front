02c0d54ccc43e0698c71652d2274f2d9d22245a7
Jairo Nicolás Gómez <jn.gomezm1@uniandes.edu.co>
2022-06-05 19:09:47 -0500

    Update jugador.service.ts

--- src/app/jugador/jugador.service.ts
+++ src/app/jugador/jugador.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { JugadorDetail } from './jugador-detail';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +11,13 @@ export class JugadorService {
 
 private apiUrl: string = environment.baseUrl + 'jugador';
 constructor(private http: HttpClient) { }
+
+getJugadores(): Observable<JugadorDetail[]> {
+  return this.http.get<JugadorDetail[]>(this.apiUrl);
+}
+
+getApertura(id: string): Observable<JugadorDetail> {
+  return this.http.get<JugadorDetail>(this.apiUrl + "/" + id);
+}
 
 }
