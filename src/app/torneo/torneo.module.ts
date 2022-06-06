f699e6a9d4baa5d790463f646b604b17b633f3be
zjxdsr <d.rappy@uniandes.edu.co>
2022-06-05 21:35:19 -0500

    poosh torneo

--- src/app/torneo/torneo.module.ts
+++ src/app/torneo/torneo.module.ts
@@ -2,9 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TorneoListComponent } from './torneo-list/torneo-list.component';
 import { TorneoDetailComponent } from './torneo-detail/torneo-detail.component';
+import { RouterModule } from '@angular/router';
+import { TorneoRoutingModule } from './torneo-routing.module';
+
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    TorneoRoutingModule
+  ],
   exports: [TorneoListComponent],
   declarations: [TorneoListComponent, TorneoDetailComponent]
 })
